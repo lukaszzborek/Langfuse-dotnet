43a53a3b1527100c17f6f9a94aba106867d9d1b6
Łukasz Zborek <luki215@gmail.com>
2025-02-16 20:46:46 +0100

    Add score comments

--- src/Langfuse/Client/LangfuseClient.cs
+++ src/Langfuse/Client/LangfuseClient.cs
@@ -11,9 +11,16 @@ namespace zborek.Langfuse.Client;
 
 internal class LangfuseClient : ILangfuseClient
 {
-    private readonly HttpClient _httpClient;
+    private static readonly JsonSerializerOptions SerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.KebabCaseUpper) }
+    };
+
     private readonly Channel<IIngestionEvent> _channel;
     private readonly IOptions<LangfuseConfig> _config;
+    private readonly HttpClient _httpClient;
 
     public LangfuseClient(HttpClient httpClient, Channel<IIngestionEvent> channel, IOptions<LangfuseConfig> config)
     {
@@ -21,7 +28,7 @@ internal class LangfuseClient : ILangfuseClient
         _channel = channel;
         _config = config;
     }
-    
+
     public async Task IngestAsync(IIngestionEvent ingestionEvent, CancellationToken cancellationToken = default)
     {
         await IngestInternalAsync(ingestionEvent, cancellationToken);
@@ -35,41 +42,37 @@ internal class LangfuseClient : ILangfuseClient
             await IngestAsync(@event, cancellationToken);
         }
     }
-    
+
     private async Task IngestInternalAsync(IIngestionEvent ingestionEvent, CancellationToken cancellationToken)
     {
-        if(_config.Value.BatchMode)
+        if (_config.Value.BatchMode)
         {
             await _channel.Writer.WriteAsync(ingestionEvent, cancellationToken);
         }
         else
-        { 
-            var ingestionRequest = new IngestionRequest()
+        {
+            var ingestionRequest = new IngestionRequest
             {
                 Batch = [ingestionEvent]
             };
             await IngestInternalAsync(ingestionRequest);
         }
     }
-    
+
     internal async Task<IngestionResponse> IngestInternalAsync(IngestionRequest request)
     {
-        var jsonSerializerOptions = new JsonSerializerOptions
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.KebabCaseUpper) }
-        };
-        var json = JsonSerializer.Serialize(request, jsonSerializerOptions);
+        // TODO: Batch request by max 3.5MB (limit by api)
+
+        var json = JsonSerializer.Serialize(request, SerializerOptions);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
-        
+
         var response = await _httpClient.PostAsync("/api/public/ingestion", content);
-        
+
         if (!response.IsSuccessStatusCode)
         {
             throw new Exception("Failed to ingest event");
         }
-        
+
         var responseJson = await response.Content.ReadAsStringAsync();
         var ingestionResponse = JsonSerializer.Deserialize<IngestionResponse>(responseJson);
 
@@ -77,7 +80,7 @@ internal class LangfuseClient : ILangfuseClient
         {
             throw new Exception("Failed to parse ingestion response");
         }
-        
+
         return ingestionResponse;
     }
 }
--- src/Langfuse/Models/CreateScoreEvent.cs
+++ src/Langfuse/Models/CreateScoreEvent.cs
@@ -3,55 +3,52 @@ using System.Text.Json.Serialization;
 namespace zborek.Langfuse.Models;
 
 /// <summary>
-/// Create score event
+///     Create score event
 /// </summary>
 public class CreateScoreEvent : IIngestionEvent
 {
     /// <summary>
-    /// Event type
+    ///     Event type
     /// </summary>
     [JsonPropertyName("type")]
     public string Type { get; init; } = "score-create";
-    
+
     /// <summary>
-    /// Event ID
+    ///     Event ID
     /// </summary>
     [JsonPropertyName("id")]
     public string Id { get; set; }
-    
+
     /// <summary>
-    /// Date of the event
+    ///     Date of the event
     /// </summary>
     [JsonPropertyName("timestamp")]
     public string Timestamp { get; set; }
-    
-    /// <summary>
-    /// Create score event body
-    /// </summary>
-    [JsonPropertyName("body")]
-    public ScoreEventBody Body { get; set; }
 
     /// <summary>
-    /// 
+    ///     Create score event body
+    /// </summary>
+    [JsonPropertyName("body")]
+    public CreateScoreEventBody Body { get; set; }
+
+    /// <summary>
     /// </summary>
     /// <param name="body">Score event body</param>
     /// <param name="id">Score event id</param>
     /// <param name="timestamp">Score event date</param>
-    public CreateScoreEvent(ScoreEventBody body, string id, string timestamp)
+    public CreateScoreEvent(CreateScoreEventBody body, string id, string timestamp)
     {
         Body = body;
         Id = id;
         Timestamp = timestamp;
     }
-    
+
     /// <summary>
-    /// 
     /// </summary>
     /// <param name="body">Score event body</param>
     /// <param name="timestamp">Score event date</param>
-    public CreateScoreEvent(ScoreEventBody body, string timestamp) 
+    public CreateScoreEvent(CreateScoreEventBody body, string timestamp)
         : this(body, Guid.NewGuid().ToString(), timestamp)
     {
-        
     }
 }
